3eb9ffa1de40ffe4ec7eba0336c43fe01853ca25
adhit24 <adhit24@gmail.com>
2026-07-30 10:23:48 +0700

    Add entrance-only page transition animation on route change

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,13 +1,16 @@
 import { Sidebar } from "@/components/Sidebar";
 import { BottomNav } from "@/components/BottomNav";
 import { IdleLogout } from "@/components/IdleLogout";
+import { PageTransition } from "@/components/PageTransition";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen bg-slate-50">
       <IdleLogout />
       <Sidebar />
-      <div className="flex min-w-0 flex-1 flex-col pb-16 sm:pb-0">{children}</div>
+      <div className="flex min-w-0 flex-1 flex-col pb-16 sm:pb-0">
+        <PageTransition>{children}</PageTransition>
+      </div>
       <BottomNav />
     </div>
   );
